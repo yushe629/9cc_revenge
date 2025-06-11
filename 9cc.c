89409826f7ec5c53d521ce25f5f2b07339f33716
yushe <yushedev@gmail.com>
2025-06-12 00:10:18 +0900

    step6: Add unary

--- 9cc.c
+++ 9cc.c
@@ -145,6 +145,7 @@ Node *new_node_num(int val) {
 // 先に宣言していないと使えない
 Node *mul();
 Node *primary();
+Node *unary();
 
 Node *expr() {
   Node *node = mul();
@@ -160,16 +161,24 @@ Node *expr() {
 }
 
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for(;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+Node *unary() {
+  if (consume('+'))
+    return unary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), unary());
+  return primary();
 }
 
 Node *primary() {
